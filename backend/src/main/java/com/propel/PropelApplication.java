430f13a53bbdd1b17eed16aa18d41bc571fd62ec
Shiva84 <shiva91official@gmail.com>
2026-01-02 02:39:04 +0530

    start

---
+++ backend/src/main/java/com/propel/PropelApplication.java
@@ -0,0 +1,11 @@
+package com.propel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PropelApplication.class, args);
+    }
+}
